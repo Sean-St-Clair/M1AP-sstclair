f0f8684a5beb6537c85c15f7e0df07668f5ddfe2
seans <sstclair@uvm.edu>
2023-02-05 20:10:45 -0800

    Refactoring and testing. Edited floating point text prompt.

--- main.cpp
+++ main.cpp
@@ -3,20 +3,36 @@
 #include <sstream>
 #include <string>
 
-// TODO: More comments?
-
 using namespace std;
 
+/*
+ * This function takes a string and returns true if and only if it contains any whitespace characters
+ */
 bool stringContainsSpace(string input);
 
+/*
+ * This function prompts a user for input until a valid single character is entered.
+ */
 char get_char_from_user();
 
+/*
+ * This function prompts a user for input until a valid word is entered.
+ */
 string get_word_from_user();
 
+/*
+ * This function prompts a user for input until a valid sentence is entered.
+ */
 string get_sentence_from_user();
 
+/*
+ * This function prompts a user for input until a valid integer is entered.
+ */
 int get_int_from_user();
 
+/*
+ * This function prompts a user for input until a valid floating-point number is entered.
+ */
 double get_float_from_user();
 
 int main() {
@@ -45,6 +61,8 @@ char get_char_from_user() {
     string prompt = "Enter a single character: ";
     cout << prompt;
     getline(cin, input);
+
+    // Re-prompts user for input if more or less than 1 character is entered
     while (input.length() != 1) {
         if (input.length() < 1)
             cout << "No input. ";
@@ -60,6 +78,8 @@ string get_word_from_user() {
     string prompt = "Enter a single word: ";
     cout << prompt;
     getline(cin, input);
+
+    // Re-prompts user for input if it contains any whitespace characters
     while (input.length() < 1 || stringContainsSpace(input)) {
         if (input.length() < 1)
             cout << "No input. ";
@@ -75,6 +95,8 @@ string get_sentence_from_user() {
     string prompt = "Enter a sentence: ";
     cout << prompt;
     getline(cin, input);
+
+    // Re-prompts user for input if none is provided
     while (input.length() < 1) {
         cout << "No input. " << prompt;
         getline(cin, input);
@@ -95,6 +117,7 @@ int get_int_from_user() {
     ss.str(input);
     ss >> num;
 
+    // Checks whether the string stream could properly read from the whole string to an integer
     while (input.length() < 1 || ss.fail() || ss.peek() != EOF) {
         if (input.length() < 1) {
             cout << "No input. ";
@@ -102,6 +125,7 @@ int get_int_from_user() {
         ss.clear();
         excess = "";
 
+        // Re-prompts user for input
         cout << prompt;
         getline(cin, input);
         ss.str(input);
@@ -113,9 +137,9 @@ int get_int_from_user() {
 double get_float_from_user() {
     string input;
     string excess;
-    string prompt = "Enter a number: ";
+    string prompt = "Enter a floating-point number: ";
     stringstream ss;
-    double num;
+    float num;
 
     // Uses a string stream to process input into num
     cout << prompt;
@@ -123,6 +147,7 @@ double get_float_from_user() {
     ss.str(input);
     ss >> num;
 
+    // Checks whether the string stream could properly read from the whole string to a float
     while (input.length() < 1 || ss.fail() || ss.peek() != EOF) {
         if (input.length() < 1) {
             cout << "No input. ";
@@ -130,6 +155,7 @@ double get_float_from_user() {
         ss.clear();
         excess = "";
 
+        // Re-prompts user for input
         cout << prompt;
         getline(cin, input);
         ss.str(input);
